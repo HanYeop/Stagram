e940eba2d0c54ae98b17b76fb7676b2613fc6ea6
Hanyeop <super7615@naver.com>
2020-12-30 13:46:48 +0900

    메인 네비게이션 추가, 프래그먼트 정리

--- app/src/main/java/org/techtown/stagram/MainActivity.kt
+++ app/src/main/java/org/techtown/stagram/MainActivity.kt
@@ -2,10 +2,49 @@ package org.techtown.stagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import org.techtown.stagram.navigation.AlarmViewFragment
+import org.techtown.stagram.navigation.DetailViewFragment
+import org.techtown.stagram.navigation.GridFragment
+import org.techtown.stagram.navigation.UserFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    private var detailViewFragment = DetailViewFragment()
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.home ->{
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,detailViewFragment).commit()
+                return true
+            }
+            R.id.search ->{
+                var gridFragment = GridFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,gridFragment).commit()
+                return true
+            }
+            R.id.add_photo ->{
+
+                return true
+            }
+            R.id.favorite_alarm ->{
+                var alarmFragment = AlarmViewFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,alarmFragment).commit()
+                return true
+            }
+            R.id.account ->{
+                var userFragment = UserFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,userFragment).commit()
+                return true
+            }
+        }
+        return false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
+        supportFragmentManager.beginTransaction().replace(R.id.main_content,detailViewFragment).commit()
     }
 }
---
+++ app/src/main/java/org/techtown/stagram/navigation/AlarmViewFragment.kt
@@ -0,0 +1,16 @@
+package org.techtown.stagram.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.techtown.stagram.R
+
+class AlarmViewFragment : Fragment(){
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_alarm,container,false)
+        return view
+    }
+}
